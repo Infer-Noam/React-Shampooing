4048b86179a669c796472931c1b6d5d40d52771f
Infer-Noam <xd.NNoam39@idf.il>
2025-07-06 22:43:29 +0300

    Changed project names

--- TodoWebApp/src/components/TaskGrid/TaskGrid.style.ts
+++
@@ -1,8 +0,0 @@
-import type { SxProps } from "@mui/material";
-
-const gridItem: SxProps = { xs: 1, md: 5 };
-
-const gridContainer: SxProps = {
-  justifyContent: "center",
-};
-export default { gridItem, gridContainer };
