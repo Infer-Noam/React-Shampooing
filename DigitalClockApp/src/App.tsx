32775dd2d5795a93c010ef80634c5a0bab4c7006
Infer-Noam <xd.NNoam39@idf.il>
2025-07-07 12:17:01 +0300

    MyTimePicker commit

--- DigitalClockApp/src/App.tsx
+++ DigitalClockApp/src/App.tsx
@@ -3,12 +3,11 @@ import theme from "./styles/theme";
 import { ThemeProvider } from "@mui/material/styles";
 import MyTimePicker from "./components/MyTimePicker/MyTimePicker.tsx";
 
-
 function App() {
   return (
     <>
-      <ThemeProvider theme={theme}>
-          <MyTimePicker></MyTimePicker>
+      <ThemeProvider theme={theme} >
+          <MyTimePicker ></MyTimePicker>
       </ThemeProvider>
     </>
   );
